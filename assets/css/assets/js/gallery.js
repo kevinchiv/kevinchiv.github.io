333b3330d9c7103306fea5bee0a1547470bd0ad4
kchiv0403 <kchiv0403@berkeley.edu>
2018-11-27 19:15:44 -0800

    Add files via upload

---
+++ assets/css/assets/js/gallery.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $(document).scroll(function(){ 
+    const time = 600;
+    let scrollRatio = $(document).scrollTop()/window.innerHeight;
+
+    if (scrollRatio >= 2.3) {
+      for (let i = 0; i<4; i++) {
+        const index = i + 1;
+        const selectorString = "#g" + index;
+        $("#photo-gallery >" + selectorString).fadeTo(time * index, 1);
+      };
+    }
+  });
+
+
+  let rotated = [false, false, false, false, false, false, false, false,
+                  false, false, false, false, false, false, false, false];
+  
+  $(".gallery-item").click(function() {
+    const index = $(".gallery-item").index(this);
+    //const item = $(".gallery-item").get(index);
+    if (rotated[index] == false) {
+      rotated[index] = true;
+      $(this).find("img").css({'transform' : 'rotateY('+ 180 +'deg)'});  
+      $(this).find("img").css('opacity', '0');
+      $(this).find("#recipe-button").css('display', 'flex');
+      $(this).css('cursor', 'default');
+    }
+  });
+});
+
---
+++ assets/css/assets/js/typing.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$("#type-it").typed({
+		strings: ["the Definition of", "the History of", "Recipes for"],
+		typeSpeed: 50,
+		loop: true,
+		loopCount: false,
+		showCursor: false,
+	});
+});
+
+
+
